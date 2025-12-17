9051c833f232c9363e8d7ab9a2c118efe5a35868
hatim47 <hatimnoor47@gmail.com>
2025-12-17 20:17:22 +0500

    sdsadsad

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,10 +1,10 @@
 <footer class="d-footer">
     <div class="row align-items-center justify-content-between">
         <div class="col-auto">
-            <p class="mb-0">© 2024 WowDash. All Rights Reserved.</p>
+            <p class="mb-0">© {{ date('Y') }} WbSoftech. All Rights Reserved.</p>
         </div>
         <div class="col-auto">
-            <p class="mb-0">Made by <span class="text-primary-600">pixcelsthemes</span></p>
+            <p class="mb-0">Made by <span class="text-primary-600">WbSoftech</span></p>
         </div>
     </div>
 </footer>
